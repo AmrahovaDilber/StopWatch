d1af79372329e3b6d2ce1e4854e59cdbeb5acd5a
AmrahovaDilber <amrahovad911@gmail.com>
2024-07-10 13:42:02 +0400

    added watchStart

---
+++ index.js
@@ -0,0 +1,35 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+const displayTime = document.querySelector("#displayTime");
+const startBtn = document.querySelector("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const restartBtn = document.querySelector("#restartBtn");
+let timer = null;
+function stopWatch() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+    }
+    let h = hours < 10 ? "0" + hours:hours;
+    let m = minutes < 10 ?"0" + minutes:minutes
+    let s = seconds < 10 ? "0" + seconds:seconds
+  displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+
+
+function watchStart() {
+  if (timer != null) {
+    clearInterval(timer);
+  }
+
+  timer = setInterval(() => {
+    stopWatch();
+  }, 1000);
+}
+startBtn.addEventListener("click", watchStart);
+
